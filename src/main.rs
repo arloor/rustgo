35f4c4fa4808f87d32efc29462aa37909615f2e6
liuganghuan <liuganghuan@meituan.com>
2021-09-20 23:40:29 +0800

    修改环境

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{fs, env};
+
 fn main() {
-    println!("Hello, world!");
+    const PATH: &str = "/data/webapps/appenv";
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(),2);
+    let env = &args[1];
+    let content = fs::read_to_string(PATH).expect("无法读取appenv");
+    let content = content.replace("\r\n", "\n");
+    let out = modify(env, content);
+
+    fs::write(PATH, &out).expect("写入appenv失败");
+    println!("# 当前appenv为\n{}", out);
+}
+
+fn modify(env: &String, content: String) -> String {
+    let mut out = String::new();
+    for line in content.split("\n") {
+        if line.len() != 0 {
+            if line.starts_with("env=") {
+                out.push_str("env=");
+                out.push_str(env);
+            } else {
+                out.push_str(line);
+            }
+            out.push_str("\n");
+        }
+    }
+    out.push_str("\n");
+    out
 }
